239396911634ea2105cb886e451ea5722a8bac43
John <lanran5566@gmail.com>
2022-05-29 16:09:37 +0800

    Completed graphs.

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,4 +1,5 @@
 package lab11.graphs;
+import java.util.concurrent.ArrayBlockingQueue;
 
 /**
  *  @author Josh Hug
@@ -10,20 +11,54 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public boolean[] marked;
     */
 
+    int s;
+
+    int t;
+
+    Maze maze;
+
+
+
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
+        maze = m;
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
         // Add more variables here!
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
     private void bfs() {
         // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        marked[s] = true;
+        announce();
+        ArrayBlockingQueue<Integer> queue = new ArrayBlockingQueue<>(maze.V());
+
+        queue.add(s);
+        while (!queue.isEmpty()) {
+            int v = queue.poll();
+            if (v == t) {
+                return;
+            }
+            for (int w : maze.adj(v)) {
+                if (!marked[w]) {
+                    marked[w] = true;
+                    edgeTo[w] = v;
+                    distTo[w] = distTo[v] + 1;
+                    queue.add(w);
+                    announce();
+                }
+            }
+        }
+
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+        bfs();
     }
 }
 
--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -17,6 +17,20 @@ public class MazeCycles extends MazeExplorer {
     @Override
     public void solve() {
         // TODO: Your code here!
+        dfs(0, 0);
+    }
+
+    private void dfs(int v, int p) {
+        marked[v] = true;
+        announce();
+        for (int w : maze.adj(v)) {
+            if (!marked[w]) {
+                dfs(w, v);
+            }
+            if (marked[w] && w != p) {
+                edgeTo[v] = p;
+            }
+        }
     }
 
     // Helper methods go here
